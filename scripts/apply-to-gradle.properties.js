227b37d511669c173dd5a0b65aebabcfe843029b
Julian Alimin <dmastag@yahoo.com>
2022-12-20 23:40:22 +0700

    Remove enableR8

--- scripts/apply-to-gradle.properties.js
+++ scripts/apply-to-gradle.properties.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 const PLUGIN_NAME = "cordova-plugin-androidx";
-const enableR8 = "android.enableR8=false";
+// const enableR8 = "android.enableR8=false";
 const gradlePropertiesPath = "./platforms/android/gradle.properties";
 
 function log(message) {
@@ -18,10 +18,10 @@ function run() {
     if (gradleProperties) {
         let updatedGradleProperties = false;
         gradleProperties = gradleProperties.toString();
-        if (!gradleProperties.match(enableR8)) {
-            gradleProperties += "\n" + enableR8;
-            updatedGradleProperties = true;
-        }
+        //if (!gradleProperties.match(enableR8)) {
+        //    gradleProperties += "\n" + enableR8;
+        //    updatedGradleProperties = true;
+        //}
         if (updatedGradleProperties) {
             fs.writeFileSync(gradlePropertiesPath, gradleProperties, 'utf8');
             log("Updated gradle.properties to enable AndroidX");
